4958fe8c0336a38294a1434183b93670f16f205e
HayatoUeda <H.U@HU.local>
2021-08-14 00:40:31 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '7adad3db6f58dc73004aff11c00a80056743df45aad0a21e9a18c482f09ca693b841d4b36807e093c5d9d567ce8aeadfcbffab88681a820284526340e2d807fe'
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  devise_for :users, controllers: {registrations: 'users/registrations'}
+  resources :users
+  resources :profiles, only: [:show, :new, :edit, :create, :update]
+  resources :reservations, only: [:index]
+  resources :rooms do
+    collection do
+      get "search"
+    end
+  end
+  resources :rooms do
+    resources :reservations, except: :index
+  end
+  root 'top#index'
+end
